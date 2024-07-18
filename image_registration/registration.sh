7e6145ff7a54b8039edb46b463ee6e15d16e9e81
ale-ch <alessiochiodin@hotmail.com>
2024-07-18 17:51:35 +0200

    Update registration.sh

--- image_registration/registration.sh
+++ image_registration/registration.sh
@@ -5,12 +5,13 @@ root_dir="/hpcnfs"
 
 current_dir="${root_dir}/scratch/DIMA/chiodin/repositories/image_registration_pipeline"
 data_dir="${root_dir}/techunits/imaging/work/ATTEND/achiodin/image_registration_pipeline/image_registration/data"
+output_root_path="${root_dir}/techunits/imaging/work/ATTEND/achiodin/image_registration/output"
 
 cd "$current_dir"
 
 python "${current_dir}/shared/update_paths.py" \
     --input-root-path "${data_dir}" \
-    --output-root-path "${current_dir}/image_registration/output/registered_stitched_images" \
+    --output-root-path "${output_root_path}" \
     --logs-dir "${current_dir}/image_registration/logs/io" \
     --cur-logs-dir "${current_dir}/image_registration/logs/io/current" \
     --backup-dir "${current_dir}/image_registration/logs/backups" \
@@ -20,8 +21,8 @@ python "${current_dir}/shared/update_paths.py" \
 
 python "${current_dir}/image_registration/src/register_images.py" \
     --cur-logs-dir "${current_dir}/image_registration/io/logs/current/" \
-    --root-mappings-dir "${current_dir}/image_registration/output/mappings" \
-    --root-registered-crops-dir "${current_dir}/image_registration/output/registered_crops" \
+    --root-mappings-dir "${output_root_path}/mappings" \
+    --root-registered-crops-dir "${output_root_path}/registered_crops" \
     --fixed-image-pattern CYCLE_1 \
     --crop-width-x 500 \
     --crop-width-y 500 \

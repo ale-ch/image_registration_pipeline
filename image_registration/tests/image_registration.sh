b9803f28c9ac3ef0359449977dba44c8a8dfacfd
ale-ch <alessiochiodin@hotmail.com>
2024-07-18 23:46:51 +0200

    Delete image_registration directory

--- image_registration/tests/image_registration.sh
+++
@@ -1,7 +0,0 @@
-python update_paths.py
-
-python /hpcnfs/techunits/imaging/work/ATTEND/achiodin/image_registration_pipeline/image_registration/src\ 
-           --cur_input_paths_file '/hpcnfs/techunits/imaging/work/ATTEND/achiodin/image_registration/cur_input_paths.txt' \
-           --cur_output_paths_file '/hpcnfs/techunits/imaging/work/ATTEND/achiodin/image_registration/cur_output_paths.txt' \ 
-           --root_mappings_dir '/hpcnfs/techunits/imaging/work/ATTEND/achiodin/image_registration/output/mappings' \ 
-           --root_registered_crops_dir '/hpcnfs/techunits/imaging/work/ATTEND/achiodin/image_registration/output/registered_crops'

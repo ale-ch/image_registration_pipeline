5390fb7898e366693ed368d1c5bff7d002d951cb
Chiodin Alessio <ieo7086@cn07.cluster.loc>
2024-08-26 07:40:21 +0200

    Added test input parameters.

--- bin/io_test.py
+++ bin/io_test.py
@@ -12,6 +12,9 @@ def main(args):
     }
 
     df = pd.DataFrame(data)
+
+    df['test_1'] = args.something_1
+    df['test_2'] = args.something_2
     df.to_csv(args.export_path, index=False)        
 
 
@@ -19,6 +22,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process input and output directories.')
     parser.add_argument('--export-path', type=str, required=True,
                         help='Path where file will be saved.')
+    parser.add_argument('--something_1', type=str, required=True)
+    parser.add_argument('--something_2', type=str, required=True)
     
     args = parser.parse_args()
     main(args)

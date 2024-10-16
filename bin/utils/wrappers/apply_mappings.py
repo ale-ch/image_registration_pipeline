9e9b937fd92d02c222fe504d77e690bdf16d9bc0
Chiodin Alessio <ieo7086@cn07.cluster.loc>
2024-10-16 11:49:24 +0200

    Parallelized crop transformations

--- bin/utils/wrappers/apply_mappings.py
+++ bin/utils/wrappers/apply_mappings.py
@@ -2,75 +2,70 @@
 
 import os
 import numpy as np
+import re
+import gc
 from utils.pickle_utils import save_pickle, load_pickle
 from utils.image_mapping import apply_mapping
+from concurrent.futures import ProcessPoolExecutor
 
-def apply_mappings(mappings, moving_crops, method='dipy', checkpoint_dir=None):
+def process_crop(mapping_file, moving_file, checkpoint_dir=None):
     """
     Apply affine and diffeomorphic mappings to a set of image crops and save/load results from checkpoints.
 
     Parameters:
-        mappings (list): List of tuples containing affine and diffeomorphic mappings.
-        moving_crops (list): List of tuples containing crop indices and image data.
-        method (str): The mapping method to use ('cv2' or 'dipy'). Default is 'dipy'.
+        mapping_file (list): Path to diffeomorphic mapping file.
+        moving_file (list): Path to moving crop file.
         checkpoint_dir (str): Directory to save/load checkpoint files.
+    """
+    match = re.search(r'\d+_\d+', mapping_file)
+    idx = match.group(0)
+
+    n_channels = 3  # Assuming all crops have the same number of channels
+    for ch in range(n_channels):
+        checkpoint_path = os.path.join(checkpoint_dir, f'registered_split_{idx}_{ch}.pkl')
+        if not os.path.exists(checkpoint_path):
+            moving_crop = load_pickle(moving_file)
+            mapping = load_pickle(mapping_file)
+
+            mov_crop = moving_crop[1][:, :, ch]  # Extract the specific channel of the moving crop
+            mov_crop_idx = moving_crop[0]  # Get the index of the crop
+
+            # Check for single valued array (such as white border)
+            if not len(np.unique(mov_crop)) == 1:
+            # Apply mappings
+                mapped_image_indexed = (mov_crop_idx + (ch,), apply_mapping(mapping, mov_crop, method='dipy'))
+            else:
+            # Return crop as is
+                mapped_image_indexed = (mov_crop_idx + (ch,), mov_crop)
+            
+            del moving_crop, mapping
+            gc.collect()
+
+            # Save checkpoint
+            save_pickle(mapped_image_indexed, checkpoint_path)
+            print(f"Saved checkpoint for i={mov_crop_idx}")
+
+
+def apply_mappings(mapping_files, moving_files, checkpoint_dir, max_workers=None):
+    """
+    Compute affine and diffeomorphic mappings between fixed and moving image crops in parallel.
+
+    Parameters:
+        mapping_files (list): List of filenames for the mapping.
+        moving_files (list): List of filenames for the moving crops.
+        checkpoint_dir (str): Directory to save/load checkpoint files.
+        max_workers (int, optional): Maximum number of workers for parallel processing.
 
     Returns:
-        list: List of tuples containing crop indices and the registered image data.
+        list: List of mappings corresponding to each crop, or None for mismatched shapes.
     """
     if checkpoint_dir is not None:
         # Create checkpoint directory if it doesn't exist
         os.makedirs(checkpoint_dir, exist_ok=True)
         
-    registered_crops = []
-    n_channels = moving_crops[0][1].shape[2]  # Assuming all crops have the same number of channels
-
-    if method == 'cv2':
-        # Apply cv2 mappings
-        mapping = mappings  # Assuming 'mappings' is a direct mapping in cv2 case
-        for i, crop in enumerate(moving_crops):
-            for ch in range(n_channels):
-                mov_crop = crop[1][:, :, ch]  # Extract the specific channel of the moving crop
-                mov_crop_idx = crop[0]  # Get the index of the crop
-        
-                # Apply mappings
-                mapped_image_indexed = (mov_crop_idx + (ch,), apply_mapping(mapping, mov_crop, method=method))
-        
-                registered_crops.append(mapped_image_indexed)
-
-    elif method == 'dipy':
-        # Apply dipy mappings
-        for i, mapping in enumerate(mappings):
-            for ch in range(n_channels):
-                mov_crop = moving_crops[i][1][:, :, ch]  # Extract the specific channel of the moving crop
-                mov_crop_idx = moving_crops[i][0]  # Get the index of the crop
-
-                if checkpoint_dir is not None:
-                    # Save checkpoint
-                    save_pickle(mapped_image_indexed, checkpoint_filename)
-                    print(f"Saved checkpoint for i={mov_crop_idx}")
-    
-                if checkpoint_dir is not None:
-                    # Construct checkpoint filename
-                    checkpoint_filename = os.path.join(checkpoint_dir, f'registered_split_{mov_crop_idx[0]}_{mov_crop_idx[1]}_{ch}.pkl')
-                    
-                    if os.path.exists(checkpoint_filename):
-                        # Load checkpoint if it exists
-                        mapped_image_indexed = load_pickle(checkpoint_filename)
-                        print(f"Loaded checkpoint for i={mov_crop_idx}")
-                        registered_crops.append(mapped_image_indexed)
-                        continue
-                
-                # Check for single valued array (such as white border)
-                if not len(np.unique(mov_crop)) == 1:
-                # Apply mappings
-                    mapped_image_indexed = (mov_crop_idx + (ch,), apply_mapping(mapping, mov_crop, method=method))
-                else:
-                # Return crop as is
-                    mapped_image_indexed = (mov_crop_idx + (ch,), mov_crop)
-    
-                if checkpoint_dir is not None:
-                    # Save checkpoint
-                    save_pickle(mapped_image_indexed, checkpoint_filename)
-                    print(f"Saved checkpoint for i={mov_crop_idx}")
+    # Use ProcessPoolExecutor for parallel processing
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        # Submit tasks for each crop to be processed in parallel
+        for moving_file, mapping_file,  in zip(moving_files, mapping_files):
+            executor.submit(process_crop, mapping_file, moving_file, checkpoint_dir)
 

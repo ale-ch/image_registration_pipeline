b757a14ea34442dd33027feb736da9e2e9ba48a7
Chiodin Alessio <ieo7086@cn07.cluster.loc>
2024-10-16 11:56:04 +0200

    Optimized I/O

--- bin/utils/wrappers/compute_mappings.py
+++ bin/utils/wrappers/compute_mappings.py
@@ -3,6 +3,8 @@
 import os
 import numpy as np
 import logging
+import re
+import gc
 from utils import logging_config
 from concurrent.futures import ProcessPoolExecutor
 from utils.pickle_utils import load_pickle, save_pickle
@@ -27,21 +29,18 @@ def process_crop(fixed_file, moving_file, current_crops_dir_fixed, current_crops
     Returns:
         mapping_diffeomorphic: The computed or loaded diffeomorphic mapping, or None if shapes do not match.
     """
-    # Load the fixed and moving crops from their respective files
-    fixed_crop = load_pickle(os.path.join(current_crops_dir_fixed, fixed_file))
-    moving_crop = load_pickle(os.path.join(current_crops_dir_moving, moving_file))
+    match = re.search(r'\d+_\d+', fixed_file)
+    idx = match.group(0)
 
     # Construct the checkpoint path for storing/loading mappings
-    checkpoint_path = os.path.join(checkpoint_dir, f'mapping_{fixed_crop[0][0]}_{fixed_crop[0][1]}.pkl')
-    
-    if os.path.exists(checkpoint_path):
-        # Load mapping from checkpoint if it exists
-        mapping_diffeomorphic = load_pickle(checkpoint_path)
-        logger.info(f"Loaded checkpoint for i={fixed_crop[0][0]}_{fixed_crop[0][1]}")
-    else:   
+    checkpoint_path = os.path.join(checkpoint_dir, f'mapping_{idx}.pkl')
+    if not os.path.exists(checkpoint_path):
+        fixed_crop = load_pickle(os.path.join(current_crops_dir_fixed, fixed_file))
+        moving_crop = load_pickle(os.path.join(current_crops_dir_moving, moving_file))  
+
         # Check for shape mismatch
         if fixed_crop[1][:,:,2].shape != moving_crop[1][:,:,2].shape:
-            logger.error(f"Shape mismatch for crops at indices {fixed_crop[0]} and {moving_crop[0]}.")
+            logger.error(f"Shape mismatch for crops at indices {idx}.")
             return None
 
         # Check for single valued crops (white areas)
@@ -51,11 +50,12 @@ def process_crop(fixed_file, moving_file, current_crops_dir_fixed, current_crops
             # Compute the diffeomorphic mapping
             mapping_diffeomorphic = compute_diffeomorphic_mapping_dipy(fixed_crop[1][:, :, 2], moving_crop[1][:, :, 2])
         
+        del fixed_crop, moving_crop
+        gc.collect()
+        
         # Save the computed mapping to a checkpoint
         save_pickle(mapping_diffeomorphic, checkpoint_path)
-        logger.info(f"Saved checkpoint for i={fixed_crop[0][0]}_{fixed_crop[0][1]}")
-        
-    return mapping_diffeomorphic
+        logger.info(f"Saved checkpoint for i={idx}")
 
 def compute_mappings(fixed_files, moving_files, current_crops_dir_fixed, current_crops_dir_moving, checkpoint_dir, max_workers=None):
     """
@@ -72,23 +72,15 @@ def compute_mappings(fixed_files, moving_files, current_crops_dir_fixed, current
     Returns:
         list: List of mappings corresponding to each crop, or None for mismatched shapes.
     """
-    # Create checkpoint directory if it doesn't exist
-    os.makedirs(checkpoint_dir, exist_ok=True)
-
-    mappings = []
+    if checkpoint_dir is not None:
+        # Create checkpoint directory if it doesn't exist
+        os.makedirs(checkpoint_dir, exist_ok=True)
 
     # Use ProcessPoolExecutor for parallel processing
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         # Submit tasks for each crop to be processed in parallel
-        futures = [
+        for fixed_file, moving_file in zip(fixed_files, moving_files): 
             executor.submit(process_crop, fixed_file, moving_file, current_crops_dir_fixed, current_crops_dir_moving, checkpoint_dir)
-            for fixed_file, moving_file in zip(fixed_files, moving_files)
-        ]
+    
 
-        # Collect the results as they complete
-        for future in futures:
-            result = future.result()
-            mappings.append(result)  # Add the result to the mappings list
-
-    return mappings  # Return the list of mappings
 
